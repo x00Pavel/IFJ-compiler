d8c377f7d9bf316bc13c23880ef87456461fad7e
SnailRabbids <36141552+SnailRabbids@users.noreply.github.com>
2019-10-11 14:40:40 +0200

    Add files via upload

--- proj/ifj2019.c
+++ proj/ifj2019.c
@@ -4,29 +4,108 @@
 #include "errors.h"
 #include "scaner.h"
 
-void scaner(char *str){
+// je nutne udelat v headeru prototipy
+
+void scaner(char *str, token_ptr ptr){
 int c;
 	for( int i = 0; (c = getchar()) != ' ' || c != '\n' || c != EOF ; i++)
 	{
 	str[i] = c;
 	}
-	check(str, &c);
+	check(str, &c, ptr);
 	//scaner(str);
 }
 
-void check(char *str, int *c){
-for (int i = 0; i < 7; i++){ // analogicke funcke pro ostatni typy atributu
-	if( *str == *key_word_arr[i]){
-		printf("TOKEN+\n"); // sem pridame funcki pro vytvoreni tokenu
+void check(char *str, int *c, token_ptr ptr){
+for( int i = 0; i < 7; i++){ // analogicke funcke pro ostatni typy atributu
+	if(strcmp(*str,*key_word_arr[i])){
+		create_token_kw(str, ptr, &i); // sem pridame funcki pro vytvoreni tokenu
 		}
 	}
-	if(c != EOF){ // kdyz nejsme na konci souboru, tak hledame dalsi slovo
-	scaner(str);
+for( int  i = 0; i < 22; i++){
+	if(strcmp(*str,"+")){
+		ptr -> type = TOKEN_SUM;
+		ptr -> attribute = NULL; // muzene vynechat
 	}
+	if(strcmp(*str,"*")){
+		ptr -> type = TOKEN_MULTIPLY;
+		ptr -> attribute = NULL;
+	}
+	if(strcmp(*str,"/")){
+		ptr -> type = TOKEN_DIVISION;
+		ptr -> attribute = NULL;
+	}
+	if(strcmp(*str,"=")){
+		ptr -> type = TOKEN_ASSIGN;
+		ptr -> attribute = NULL; // muzene vynechat
+	}
+	if(strcmp(*str,"==")){
+		ptr -> type = TOKEN_EQUAL;
+		ptr -> attribute = NULL;
+	}
+	if(strcmp(*str,"!=")){
+		ptr -> type = TOKEN_NOT_EQUAL;
+		ptr -> attribute = NULL;
+	}
+		if(strcmp(*str,">")){
+		ptr -> type = TOKEN_GREATER;
+		ptr -> attribute = NULL; // muzene vynechat
+	}
+	if(strcmp(*str,"<")){
+		ptr -> type = TOKEN_LESS;
+		ptr -> attribute = NULL;
+	}
+	if(strcmp(*str,">=")){
+		ptr -> type = TOKEN_GREATER_EQ;
+		ptr -> attribute = NULL;
+	}
+	if(strcmp(*str,"<=")){
+		ptr -> type = TOKEN_LESS_EQ;
+		ptr -> attribute = NULL; // muzene vynechat
+	}
+	if(strcmp(*str,"EOF")){
+		ptr -> type = TOKEN_EOF;
+		ptr -> attribute = NULL;
+	}
+	if(strcmp(*str,"\n")){
+		ptr -> type = TOKEN_EOL;
+		ptr -> attribute = NULL;
+	}
+	if(strcmp(*str," ")){
+		ptr -> type = TOKEN_EMPTY;
+		ptr -> attribute = NULL; // muzene vynechat
+	}
+	if(strcmp(*str,"(")){
+		ptr -> type = TOKEN_L_BRACKET;
+		ptr -> attribute = NULL;
+	}
+	if(strcmp(*str,")")){
+		ptr -> type = TOKEN_R_BRACKET;
+		ptr -> attribute = NULL;
+	}
+	if(strcmp(*str,".")){
+		ptr -> type = TOKEN_DOT;
+		ptr -> attribute = NULL; // muzene vynechat
+	}
+	if(strcmp(*str,",")){
+		ptr -> type = TOKEN_COMA;
+		ptr -> attribute = NULL;
+	}
+	if(strcmp(*str,";")){
+		ptr -> type = TOKEN_SEMICOLON;
+		ptr -> attribute = NULL;
+	}
+	//at , asi podarzi se nam tento kus kodu zoptimizovat
+	
+}	
+	//if(c != EOF){ // kdyz nejsme na konci souboru, tak hledame dalsi slovo
+	//scaner(str);
+	//} // dalsi slovo bude hledat PARSER
 }
 
-void create_token(){
-
+void create_token_kw(char *str, token_ptr ptr, int *i){
+	ptr -> type = TOKEN_KEY_WORD;
+	ptr -> attribute.key_word = i;
 }
 
 int main(int arc, char **argv)
@@ -40,7 +119,8 @@ int main(int arc, char **argv)
         return ERR_OTHER;
     }
     /* Lexer */
-    	scaner(str);
+    token_ptr ptr;
+    	scaner(str, ptr);
     fclose(file);
     /* Syntax analyzer MAIN PROCESS */
 
