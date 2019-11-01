b04d5e21e785455739e7e8dfc16cba32bfebe48e
x00Pavel <xyadlo00@stud.fit.vutbr.cz>
2019-11-01 12:58:13 +0100

    add stack

--- proj/compiler/src/stack/c202.h
+++ proj/compiler/src/stack/c202.h
@@ -16,30 +16,23 @@
 
 #define MAX_STACK 20
 extern int STACK_SIZE; 
-/* 
- * Hodnota MAX_STACK udává skutečnou velikost statického pole pro uložení
- * hodnot zásobníku. Při implementaci operací nad ADT zásobník však
- * předpokládejte, že velikost tohoto pole je pouze STACK_SIZE. Umožní to
- * jednoduše měnit velikost zásobníku v průběhu testování. Při implementaci
- * tedy hodnotu MAX_STACK vůbec nepoužívejte. 
- */
 
-extern int solved;                      /* Indikuje, zda byla operace řešena. */
-extern int err_flag;                   /* Indikuje, zda operace volala chybu. */
+extern int solved;                     
+extern int err_flag;                 
 
-                                        /* Chybové kódy pro funkci stackError */
-#define MAX_SERR    3                                   /* počet možných chyb */
-#define SERR_INIT   1                                  /* chyba při stackInit */
-#define SERR_PUSH   2                                  /* chyba při stackPush */
-#define SERR_TOP    3                                   /* chyba při stackTop */
+                                        
+#define MAX_SERR    3                                   
+#define SERR_INIT   1                                 
+#define SERR_PUSH   2                                 
+#define SERR_TOP    3                                   
 
-                             /* ADT zásobník implementovaný ve statickém poli */
-typedef struct {
-	int arr[MAX_STACK];                             /* pole pro uložení hodnot */
-	int top;                                /* index prvku na vrcholu zásobníku */
+                            
+typedef struct stack {
+	int arr[MAX_STACK];                           
+	int top;                                
 } tStack;
 
-                                  /* Hlavičky funkcí pro práci se zásobníkem. */
+                                 
 void stackError ( int error_code );
 void stackInit ( tStack* s );
 int stackEmpty ( const tStack* s );
@@ -49,5 +42,3 @@ void stackPop ( tStack* s );
 void stackPush ( tStack* s, int c );
 
 #endif
-
-/* Konec hlavičkového souboru c202.h */
