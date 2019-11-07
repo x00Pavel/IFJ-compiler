ba45d2aead46646a2050f30a097c9fc681d31be4
x00Pavel <xyadlo00@stud.fit.vutbr.cz>
2019-11-07 17:00:19 +0100

    add some thing to sii output

--- proj/compiler/src/ifj2019.c
+++ proj/compiler/src/ifj2019.c
@@ -8,8 +8,48 @@
 #include "scaner.h"
 #include "./stack/c202.h"
 
+char *types[] = {
+
+    "TOKEN_INT",    // integer int
+    "TOKEN_FLOAT ",  // float
+    "TOKEN_STRING", // string
+
+    // Operators
+    "TOKEN_SUM ",      // +
+    "TOKEN_MINUS",    // -
+    "TOKEN_ASSIGN",   // =
+    "TOKEN_MULTIPLY", // *
+    "TOKEN_DIVISION", // /
+
+    // Relation operators
+    "TOKEN_EQUAL",       // ==
+    "TOKEN_NOT_EQUAL",   // !=
+    "TOKEN_GREATER",    // >
+    "TOKEN_LESS",       // <
+    "TOKEN_GREATER_EQ", // >=
+    "TOKEN_LESS_EQ",    // <=
+
+    // Special tokens
+    "TOKEN_EOL",      // end of line \n
+    "TOKEN_EOF",      // end of file
+    "TOKEN_KEY_WORD", // key_word_t
+    "TOKEN_EMPTY",    // "white" symbols
+
+    // Punctuations
+    "TOKEN_L_BRACKET", // (
+    "TOKEN_R_BRACKET", // )
+    "TOKEN_DOT",       // .
+    "TOKEN_COMA",      // ,
+    "TOKEN_SEMICOLON", // ;
+    "TOKEN_DDOT",      // :
+    "TOKEN_ID",
+    "TOKEN_HEX",
+    "TOKEN_INDEND", // indend
+    "TOKEN_DEDEND"  // dedend
+};
 
 int main(int arc, char **argv){
+
     (void)arc;
     /* Input file */
     FILE *file;
@@ -20,23 +60,31 @@ int main(int arc, char **argv){
     }
 
     struct token_s *token = (struct token_s *)malloc(sizeof(struct token_s));
-    // int ret_code = scanner(file, token);
-    
-    // printf("%d\n", scanner(file, token));
 
     tStack *stack = (tStack*) malloc(sizeof(tStack));
+
     stackInit(stack);
     stackPush(stack, 0);
+    
+    int ret_code = 0;
+    while (ret_code != -1){
+        ret_code = get_token(file, &token, stack);
+        if (ret_code != OK){
+            if (token->type == TOKEN_ID || token->type == TOKEN_STRING){
+                free(token->attribute.string);
+            }
+            break;
+        }
+        printf("type: %s\n", types[token->type]);
 
-    int ret_code = get_token(file, token, stack);
-    if(ret_code != 0){
-        return -1;
+        if(token->type == TOKEN_ID || token->type == TOKEN_STRING){
+            printf("--------- token ID: %s\n", token->attribute.string);
+            free(token->attribute.string);
+        }
+        
     }
-    printf("token type: %d\n", token->type);
 
-    if(token->type == TOKEN_ID){
-        free(token->attribute.string);
-    }
+
     free(stack);
     free(token);
     fclose(file);
