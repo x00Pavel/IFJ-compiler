a8946ec111bb0d3caa2a136e3f0aef1176ba502f
x00Pavel <xyadlo00@stud.fit.vutbr.cz>
2019-11-13 14:31:42 +0100

    update test

--- proj/compiler/src/ifj2019.c
+++ proj/compiler/src/ifj2019.c
@@ -70,7 +70,7 @@ main(int arc, char **argv)
     (void)arc;
     /* Input file */
     FILE *file;
-    file = fopen("test.txt", "r");
+    file = fopen(argv[1], "r");
     if (!file){
         fprintf(stderr, "Error in opening file %s\n", argv[0]);
         return ERR_OTHER;
@@ -86,17 +86,25 @@ main(int arc, char **argv)
     int ret_code = 0;
     while (ret_code != -1){
         ret_code = get_token(file, &token, stack);
-
+        
         if (ret_code != OK){
-            if (token->type == TOKEN_ID || token->type == TOKEN_STRING){
+            if (token->type == TOKEN_ID || token->type == TOKEN_STRING || (token->type ==  TOKEN_FNC)){
                 free(token->attribute.string);
             }
             break;
         }
+
+        if(token->type == TOKEN_EOF){
+#ifdef DEBUG
+            printf("%s\n", types[token->type]);
+#endif
+            break;
+        }
+
 #ifdef DEBUG
         if((token->type ==  TOKEN_KEY_WORD) || (token->type ==  TOKEN_FNC)){
             printf("%s -- %s\n", types[token->type], (token->type == TOKEN_FNC) ? token->attribute.string : kw[token->attribute.key_word]);
-            if (token->type == TOKEN_FNC){
+            if (token->type == TOKEN_FNC && token->attribute.string){
                 free(token->attribute.string);
             }
         }
@@ -104,18 +112,19 @@ main(int arc, char **argv)
             printf("%s\n", types[token->type]);
         }
 
+        
         if(token->type == TOKEN_ID || token->type == TOKEN_STRING){
             printf("--------- attribute: %s\n", token->attribute.string);
             free(token->attribute.string);
         }
 #else
-        if ((token->type == TOKEN_FNC) || (token->type == TOKEN_STRING) || (token->type == TOKEN_ID)){
+        if ((token->type == TOKEN_FNC) || (token->type == TOKEN_STRING) || (token->type == TOKEN_ID) 
+            && (token->attribute.string != NULL)){
             free(token->attribute.string);
         }
 #endif
         
     }
-
 
     free(stack);
     free(token);
