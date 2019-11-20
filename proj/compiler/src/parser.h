703778eb821ab27da066d55f44d079e2c6340cc7
x00Pavel <xyadlo00@stud.fit.vutbr.cz>
2019-11-20 14:20:56 +0100

    add documentation for doxygen

--- proj/compiler/src/parser.h
+++ proj/compiler/src/parser.h
@@ -4,9 +4,48 @@
 #include <stdio.h>
 #include "scaner.h"
 
+/**
+ * \brief
+ * 
+ * \param[in] file
+ * \param[in] token
+ * \param[in] stack
+ * 
+ * \return
+*/
 int func_for_id(FILE *file, struct token_s *token, tStack *stack);
+
+/**
+ * \brief
+ * 
+ * \param[in] file  Source file
+ * \param[in] token Input token
+ * \param[in] stack Stack with count of whitespcase
+ * 
+ * \return
+*/
 int func_for_FNC(FILE *file, struct token_s *token, tStack *stack);
+
+/**
+ * \brief
+ * 
+ * \param[in] file  Source file
+ * \param[in] token Input token
+ * \param[in] stack Stack with count of whitespcase
+ * 
+ * \return
+*/
 int func_for_atributes(FILE *file, struct token_s *token, tStack *stack);
+
+/**
+ * \brief
+ * 
+ * \param[in] file  Source file
+ * \param[in] token Input token
+ * \param[in] stack Stack with count of whitespcase
+ * 
+ * \return
+*/
 int check_next_token(FILE *file, struct token_s *token, tStack *stack);
 
 #endif 
