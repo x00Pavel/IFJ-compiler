815541f880ccee2755e432a8b2521851d3490d1f
fure0061 <rulik-a@mail.ru>
2019-11-24 00:14:22 +0100

    math  functons added

--- proj/compiler/src/codegenerator.c
+++ proj/compiler/src/codegenerator.c
@@ -20,8 +20,8 @@
 #include "parser.h"
 #include "codegenerator.h"
 #include "./stack.h"
-
-
+int i = 0;
+int t = 0;
 void create_functions(){
     fprintf(stdout,"FUNCLEN\n");
 	fprintf(stdout,"PUSHFRAME\n");
@@ -126,30 +126,93 @@ void generate_main()
     fprintf(stdout,".IFJcode19\n    ");
     fprintf(stdout,"JUMP $$main\n");
 }
+//generate end of main function
+void end_main(){
+    fprintf(stdout,"# End of main scope\n");
+	fprintf(stdout,"POPFRAME\n");
+	fprintf(stdout,"CLEARS\n");
+}
+
+
+/*
+ *
+ * DEFINE FUNCTION
+ *
+ */
+    void define_function_begin(struct token_s *token){
+        fprintf(stdout,"# Start of function\n");
+        fprintf(stdout,"LABEL $%s\n", token->attribute.string);
+        fprintf(stdout,"PUSHFRAME\n");    
+    }
+    void retval_function()
+    {
+        fprintf(stdout, "DEFVAR LF@retval\n");
+        fprintf(stdout, "MOVE LF@%%retval nil@nil\n");
+    }
+    void def_function_call(struct token_s *token, int *counter, char *s){
+        //DEFVAR LF@param1
+        //MOVE LF@param1  LF@%1     
+        
+        fprintf(stdout, "DEFVAR %s@%s%d\n",s,token->attribute.string, *counter);
+        fprintf(stdout, "MOVE %s@%s%d %s@%%d\n",s,token->attribute.string , *counter); 
+    }
+
+    void def_function_end(){
+        fprintf(stdout, "POPFRAME\n");
+        fprontf(stdout, "RETURN\n");
+    }
+/*
+ *
+ * END OF DEFINE FUNCTION
+ * 
+*/
 
 //Define variables in all scopes
 void define_variable_GF(struct token_s *token, char *s){
     fprintf(stdout, "DEFVAR %s@%s\n",s,token->attribute.string);   
 }
+
 //Assign value to variablei in all scopes
 void assign_to_variable(struct token_s *token,struct token_s *token_a, char *s)
 {//MOVE TF@%1 int@10
       switch (token->type)
     {  
         case TOKEN_INT:
-            fprintf(stdout, "MOVE %s@%%s int@%d\n",s,token->attribute.string, token_a->attribute.int_val); 
+            fprintf(stdout, "MOVE %s@%%%s int@%d\n",s,token->attribute.string, token_a->attribute.int_val); 
             break;
         case TOKEN_FLOAT:
-            fprintf(stdout, "MOVE %s@%%s float@%f\n",s,token->attribute.string, token_a->attribute.float_val); 
+            fprintf(stdout, "MOVE %s@%%%s float@%f\n",s,token->attribute.string, token_a->attribute.float_val); 
             break;
         case TOKEN_STRING:
+            fprintf(stdout, "MOVE %s@%%%s %s@%s\n",s,token->attribute.string, s, token_a->attribute.string);
         case TOKEN_ID:
-            fprintf(stdout, "MOVE %s@%%s string@%s\n",s,token->attribute.string, token_a->attribute.string);
+            fprintf(stdout, "MOVE %s@%%%s string@%s\n",s,token->attribute.string, token_a->attribute.string);
             break;
         default:
             break;  
     }  
 }
+
+// void change_int_and_float(struct token_s *token, struct token_s *token_a)
+// {
+//     if (token->type == TOKEN_FLOAT && token_a->type == TOKEN_INT)
+// 	{
+// 		fprintf(stdout, "FLOAT2R2EINT TF@%%retval TF@%%retval");
+//     }
+// 	if (token->type == TOKEN_INT && token->type == TOKEN_FLOAT)
+// 	{
+// 		fprintf(stdout,"INT2FLOAT TF@%%retval TF@%%retval");
+// 	}
+// }
+
+void call_function(struct token_s *token)
+{   
+    fprintf(stdout,"CALL $%s", token->attribute.string);
+}
+
+
+
+
 
 void token_return(struct token_s *token, char *s)
 {
@@ -217,11 +280,7 @@ void call_function(struct token_s *token)
     printf("CALL $%s", token->attribute.string);
 }
 
-void retval_function()
-{
-    fprintf(stdout, "DEFVAR LF@retval\n");
-    fprintf(stdout, "MOVE LF@%%retval nil@nil\n");
-}
+
 void retval_assign_function(struct token_s *token)
 {
     fprintf(stdout, "MOVE LF@%s TF@%%retval",token->attribute.string);
@@ -237,36 +296,7 @@ void retval_assign_function(struct token_s *token)
 
 
 
-/*
- *
- * DEFINE FUNCTION
- *
- */
-void define_function_begin(struct token_s *token)//1
-{
-    fprintf(stdout,"# Start of function\n");
-    fprintf(stdout,"LABEL $%s\n", token->attribute.string);
-    fprintf(stdout,"PUSHFRAME\n");    
-}
 
-void def_function_call(struct token_s *token, int *counter, char *s)//def char *s = TF
-{   //DEFVAR LF@param1
-    //MOVE LF@param1  LF@%1     
-    
-    fprintf(stdout, "DEFVAR %s@%s%d\n",s,token->attribute.string, *counter);
-    fprintf(stdout, "MOVE %s@%s%d %s@%%d\n",s,token->attribute.string , *counter); 
-}
-
-void def_function_end()
-{
-    fprintf(stdout, "POPFRAME\n");
-    fprontf(stdout, "RETURN\n");
-}
-/*
- *
- * END DEFINE 
- * 
-*/
 
 
 
@@ -279,10 +309,11 @@ void print_function(struct token_s *token_write, char *s)
 
 
 
-void generate_while_head()
-{
+void generate_while_head_1()
+{   
+    i++;
     fprintf(stdout, "#WHILE CYCLE\n");
-    fprintf(stdout,"LABEL while\n");
+    fprintf(stdout,"LABEL while%d\n", i);
 }
 
 void generate_while_head(struct token_s *token, char *s){
@@ -314,135 +345,335 @@ void generate_while_head(struct token_s *token, char *s){
 
 }
 void generate_while_end(struct token_s *token){
-    fprintf(stdout,"JUMP WHILE\n");
-    fprintf(stdout,"LABEL %s\n", token->attribute.string);
+    
+    fprintf(stdout,"JUMP while%d\n", i);
+    fprintf(stdout,"LABEL $EXIT%d\n", i);
 }
 
 
 
 
 void generate_if_head(){
-    fprintf(stdout,"#Endif\n");
+    fprintf(stdout,"#IF START\n");
 }
-void generate_if_start(struct token_s *token, char *s){ 
+//void generate_if_start(struct token_s *token, char *s){ 
     //if a == a
     //if a != b
     //if a >= b
     //if a <= b
     //if a < b
     //if a > b
-    fprintf(stdout,"DEFVAR %s@res\n",s);//res helps us to check if podminka je splnena
-    fprintf(stdout,"DEFVAR %s@returnvalue\n",s);
-    fprintf(stdout,"DEFVAR %s@returnvalue1\n",s);
-    switch (token->type)
-    {
-    case TOKEN_FLOAT :
-        fprintf(stdout,"MOVE %s@returnvalue float@%f\n",s, token->attribute.float_val);
-        break;
-    case TOKEN_INT :
-        fprintf(stdout,"MOVE %s@returnvalue int@%d\n",s, token->attribute.int_val);
-        break;
-    case TOKEN_STRING:
-        fprintf("MOVE %s@returnvalue string@%s\n",s, token->attribute.string);
-        break;
-    case TOKEN_ID:
-        fprintf("MOVE %s@returnvalue %s@%s\n",s, s,token->attribute.string);
-        break;
+//     fprintf(stdout,"DEFVAR %s@res\n",s);//res helps us to check if podminka je splnena
+//     fprintf(stdout,"DEFVAR %s@returnvalue\n",s);
+//     fprintf(stdout,"DEFVAR %s@returnvalue1\n",s);
+//     switch (token->type)
+//     {
+//     case TOKEN_FLOAT :
+//         fprintf(stdout,"MOVE %s@returnvalue float@%f\n",s, token->attribute.float_val);
+//         break;
+//     case TOKEN_INT :
+//         fprintf(stdout,"MOVE %s@returnvalue int@%d\n",s, token->attribute.int_val);
+//         break;
+//     case TOKEN_STRING:
+//         fprintf("MOVE %s@returnvalue string@%s\n",s, token->attribute.string);
+//         break;
+//     case TOKEN_ID:
+//         fprintf("MOVE %s@returnvalue %s@%s\n",s, s,token->attribute.string);
+//         break;
     
-    default:
-        break;
+//     default:
+//         break;
+//     }
+// }
+// void select_operator(struct token_s *token){
+//     switch (token->type)
+//     {
+//     case TOKEN_EQUAL:
+//         fprintf(stdout,"EQ ");
+//         break;
+//      case TOKEN_NOT_EQUAL:
+//         fprintf(stdout,"NOT EQ ");
+//         break;
+//     case TOKEN_GREATER:
+//         fprintf(stdout,"GT ");
+//         break;
+//     case TOKEN_GREATER_EQ:
+//         fprintf(stdout,"EQ AND GT ");
+//         break;
+//     case TOKEN_LESS:
+//        fprintf(stdout,"LT" );
+//         break;
+//     case TOKEN_LESS_EQ:
+//        fprintf(stdout,"LT AND EQ ");
+//         break;
+    
+//     default:
+//         break;
+//     }
+    
+// }
+// void returnvalue1 (struct token_s *token, char *s){
+//     switch (token->type) 
+//     {
+//     case TOKEN_FLOAT :
+//         fprintf(stdout,"MOVE %s@returnvalue1 float@%f\n",s, token->attribute.float_val);
+//         break;
+//     case TOKEN_INT :
+//         fprintf(stdout,"MOVE %s@returnvalue1 int@%d\n",s, token->attribute.int_val);
+//         break;
+//     case TOKEN_STRING:
+//         fprintf("MOVE %s@returnvalue1 string@%s\n",s, token->attribute.string);
+//         break;
+//     case TOKEN_ID:
+//         fprintf("MOVE %s@returnvalue1 %s@%s\n",s, s,token->attribute.string);
+//         break;
+    
+//     default:
+//         break;
+//     }
+// }
+// void if_body(struct token_s *token, char *s){
+//     switch (token->type)
+//     {
+//     case TOKEN_FLOAT:
+//         fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
+//         break;
+//     case TOKEN_INT:
+//         fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
+//     case TOKEN_ID:
+//         fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
+//         break;
+//     case TOKEN_STRING:
+//         fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
+    
+//     default:
+//         break;
+//     }
+//     fprintf(stdout, "JUMPIFNEQ $EXIT %s@res boot@true",s);//if(a < b)
+//                                                             //  print b
+//                                                             //else
+// }                                                          //  print a
+// void if_else_body(struct token_s *token, char *s){
+
+//     switch (token->type)
+//     {
+//     case TOKEN_FLOAT:
+//         fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
+//         break;
+//     case TOKEN_INT:
+//         fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
+//     case TOKEN_ID:
+//         fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
+//         break;
+//     case TOKEN_STRING:
+//         fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
+    
+//     default:
+//         break;
+//     }
+//     fprintf(stdout, "JUMPIFNEQ $ELSE %s@res boot@true", s );
+// }
+// void _found_else()
+// {
+//     is_else++;
+//     fprintf(stdout, "LABEL %ELSE%d\n",t);
+// }
+// void if_end(){
+//     fprintf(stdout,"LABEL $EXIT\n");
+// }
+void returnvalue(struct token_s *token_left_side, char *s){ 
+    t++;
+    fprintf(stdout,"DEFVAR %s@res%d\n",s, t);//res helps us to check if podminka je splnena
+    fprintf(stdout,"DEFVAR %s@returnvalue%d\n",s,t);
+    switch (token_left_side->type)
+    {
+        case TOKEN_FLOAT :
+            fprintf(stdout,"MOVE %s@returnvalue%d float@%f\n",s,t, token_left_side->attribute.float_val);
+            break;
+        case TOKEN_INT :
+            fprintf(stdout,"MOVE %s@returnvalue%d int@%d\n",s,t, token_left_side->attribute.int_val);
+            break;
+        case TOKEN_STRING:
+            fprintf("MOVE %s@returnvalue%d string@%s\n",s,t, token_left_side->attribute.string);
+            break;
+        case TOKEN_ID:
+            fprintf("MOVE %s@returnvalue%d %s@%s\n",s,t, s,token_left_side->attribute.string);
+            break;
+        default:
+            break;
+    }
+}
+void returnvalue1(struct token_s *token_right_side, char *s){
+    fprintf(stdout,"DEFVAR %s@res%d\n",s, t);//res helps us to check if podminka je splnena
+    fprintf(stdout,"DEFVAR %s@returnvalue1%d\n",s,t);
+    switch (token_right_side->type)
+    {
+        case TOKEN_FLOAT :
+            fprintf(stdout,"MOVE %s@returnvalue1%d float@%f\n",s,t, token_right_side->attribute.float_val);
+            break;
+        case TOKEN_INT :
+            fprintf(stdout,"MOVE %s@returnvalue1%d int@%d\n",s,t, token_right_side->attribute.int_val);
+            break;
+        case TOKEN_STRING:
+            fprintf("MOVE %s@returnvalue1%d string@%s\n",s,t, token_right_side->attribute.string);
+            break;
+        case TOKEN_ID:
+            fprintf("MOVE %s@returnvalue1%d %s@%s\n",s,t, s,token_right_side->attribute.string);
+            break;
+        default:
+            break;
     }
 }
 void select_operator(struct token_s *token){
     switch (token->type)
     {
-    case TOKEN_EQUAL:
-        fprintf(stdout,"EQ ");
-        break;
-     case TOKEN_NOT_EQUAL:
-        fprintf(stdout,"NOT EQ ");
-        break;
-    case TOKEN_GREATER:
-        fprintf(stdout,"GT ");
-        break;
-    case TOKEN_GREATER_EQ:
-        fprintf(stdout,"EQ AND GT ");
-        break;
-    case TOKEN_LESS:
-       fprintf(stdout,"LT" );
-        break;
-    case TOKEN_LESS_EQ:
-       fprintf(stdout,"LT AND EQ ");
-        break;
-    
-    default:
-        break;
-    }
-    
-}
-void returnvalue1 (struct token_s *token, char *s){
-    switch (token->type)
-    {
-    case TOKEN_FLOAT :
-        fprintf(stdout,"MOVE %s@returnvalue1 float@%f\n",s, token->attribute.float_val);
-        break;
-    case TOKEN_INT :
-        fprintf(stdout,"MOVE %s@returnvalue1 int@%d\n",s, token->attribute.int_val);
-        break;
-    case TOKEN_STRING:
-        fprintf("MOVE %s@returnvalue1 string@%s\n",s, token->attribute.string);
-        break;
-    case TOKEN_ID:
-        fprintf("MOVE %s@returnvalue1 %s@%s\n",s, s,token->attribute.string);
-        break;
-    
-    default:
-        break;
+        case TOKEN_EQUAL:
+            fprintf(stdout,"EQ ");
+            break;
+        case TOKEN_NOT_EQUAL:
+            fprintf(stdout,"NOT EQ ");
+            break;
+        case TOKEN_GREATER:
+            fprintf(stdout,"GT ");
+            break;
+        case TOKEN_GREATER_EQ:
+            fprintf(stdout,"EQ AND GT ");
+            break;
+        case TOKEN_LESS:
+        fprintf(stdout,"LT" );
+            break;
+        case TOKEN_LESS_EQ:
+        fprintf(stdout,"LT AND EQ ");
+            break;
+        default:
+            break;
     }
 }
 void if_body(struct token_s *token, char *s){
     switch (token->type)
     {
-    case TOKEN_FLOAT:
-        fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
-        break;
-    case TOKEN_INT:
-        fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
-    case TOKEN_ID:
-        fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
-        break;
-    case TOKEN_STRING:
-        fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
-    
-    default:
-        break;
+        case TOKEN_FLOAT:
+            fprintf(stdout,"%s@res %s@returnvalue%d %s@returnvalue1%d",s,t,s,s,t);
+            break;
+        case TOKEN_INT:
+            fprintf(stdout,"%s@res %s@returnvalue%d %s@returnvalue1%d",s,t,s,s,t);
+            break;
+        case TOKEN_ID:
+            fprintf(stdout,"%s@res %s@returnvalue%d %s@returnvalue1%d",s,t,s,s,t);
+            break;
+        case TOKEN_STRING:
+            fprintf(stdout,"%s@res %s@returnvalue%d %s@returnvalue1%d",s,t,s,s,t);
+            break;
+        default:
+            break;
     }
-    fprintf(stdout, "JUMPIFNEQ $EXIT %s@res boot@true",s);//if(a < b)
-                                                            //  print b
-                                                            //else
-}                                                          //  print a
-void if_else_body(struct token_s *token, char *s){
-
-    switch (token->type)
+    fprintf(stdout, "JUMPIFNEQ $B0DY-EL%d %s@res boot@true", t,s );
+}
+void found_else()
+{
+    
+    fprintf(stdout, "JUMP $EXIT%d\n",t);
+    fprintf(stdout, "LABEL B0DY-EL%d\n",t);
+}
+void dedend_of_if(int *is_else){
+    if(is_else == 0)
     {
-    case TOKEN_FLOAT:
-        fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
-        break;
-    case TOKEN_INT:
-        fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
-    case TOKEN_ID:
-        fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
-        break;
-    case TOKEN_STRING:
-        fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
-    
-    default:
-        break;
+        fprintf("BODY_EL%d\n",t);
     }
-    fprintf(stdout, "JUMPIFNEQ $ELSE %s@res boot@true", s );
-}
-void if_end(){
-    fprintf(stdout,"LABEL $EXIT\n");
+    else
+    {
+        fprintf("LABEL EXIT%d\n",t);  
+    }
+    
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+void stack_operation(struct token_s *token)
+{
+	switch (token->type)
+	{
+		case TOKEN_SUM:
+			fprintf(stdout,"ADDS\n");
+			break;
+
+		case TOKEN_MINUS:
+			fprintf(stdout,"SUBS\n");
+			break;
+
+		case TOKEN_MULTIPLY:
+			fprintf(stdout,"MULS\n");
+			break;
+
+		case TOKEN_DIVISION:
+			fprintf(stdout,"DIVS\n");
+			break;
+
+		case TOKEN_DIV_INT:
+			fprintf(stdout,"POPS GF@%%tmp_op1\n");
+			fprintf(stdout,"INT2FLOATS\n");
+			fprintf(stdout,"PUSHS GF@%%tmp_op1\n");
+			fprintf(stdout,"INT2FLOATS\n");
+			fprintf(stdout,"DIVS\n");
+			fprintf(stdout,"FLOAT2INTS\n");
+			break;
+
+		case TOKEN_EQUAL:
+			fprintf(stdout,"EQS\n");
+			break;
+            
+
+		case TOKEN_NOT_EQUAL:
+			ADD_INST("EQS\n");
+			ADD_INST("NOTS\n");
+			break;
+
+		case TOKEN_LESS_EQ:
+			ADD_INST("POPS GF@%tmp_op1");
+			ADD_INST("POPS GF@%tmp_op2");
+			ADD_INST("PUSHS GF@%tmp_op2");
+			ADD_INST("PUSHS GF@%tmp_op1");
+			ADD_INST("LTS");
+			ADD_INST("PUSHS GF@%tmp_op2");
+			ADD_INST("PUSHS GF@%tmp_op1");
+			ADD_INST("EQS");
+			ADD_INST("ORS");
+			break;
+
+		case TOKEN_LESS:
+			ADD_INST("LTS");
+			break;
+
+		case TOKEN_GREATER_EQ:
+			ADD_INST("POPS GF@%tmp_op1");
+			ADD_INST("POPS GF@%tmp_op2");
+			ADD_INST("PUSHS GF@%tmp_op2");
+			ADD_INST("PUSHS GF@%tmp_op1");
+			ADD_INST("GTS");
+			ADD_INST("PUSHS GF@%tmp_op2");
+			ADD_INST("PUSHS GF@%tmp_op1");
+			ADD_INST("EQS");
+			ADD_INST("ORS");
+			break;
+
+		case TOKEN_GREATER:
+			ADD_INST("GTS");
+			break;
+
+		default:
+			break;
+	}
+
+	return true;
+}
