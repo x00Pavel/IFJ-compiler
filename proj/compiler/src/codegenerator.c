20a1f75b4565267768e69843fbbc3371ca102377
fure0061 <rulik-a@mail.ru>
2019-11-23 15:23:11 +0100

    if added

--- proj/compiler/src/codegenerator.c
+++ proj/compiler/src/codegenerator.c
@@ -22,106 +22,102 @@
 #include "./stack.h"
 
 
-#define FUNCTION_LENGTH														\
-	"\n LABEL $length\n"													\
-	"PUSHFRAME\n"															\
-	"DEFVAR LF@%%retval\n"													\
-	"STRLEN LF@%%retval LF@%%0\n"											\
-	"POPFRAME\n"															\
-	"RETURN\n"
-
-
-#define FUNCTION_SUBSTR														\
-	"LABEL $substr\n"														\
-	"PUSHFRAME\n"															\
-	"DEFVAR LF@%%retval\n"													\
-	"MOVE LF@%%retval string@\n"											\
-	"DEFVAR LF@length_str\n"												\
-	"CREATEFRAME\n"													     	\
-	"DEFVAR TF@%%0\n"														\
-	"MOVE TF@%%0 LF@%%0\n"													\
-	"CALL $length\n"														\
-	"MOVE LF@length_str TF@%%retval\n"										\
-	"DEFVAR LF@ret_cond\n"													\
-	"LT LF@ret_cond LF@length_str int@0\n"									\
-	"JUMPIFEQ $substr$return LF@ret_cond bool@true\n"						\
-	"EQ LF@ret_cond LF@length_str int@0\n"									\
-	"JUMPIFEQ $substr$return LF@ret_cond bool@true\n"						\
-	"LT LF@ret_cond LF@%%1 int@0\n"											\
-	"JUMPIFEQ $substr$return LF@ret_cond bool@true\n"						\
-	"EQ LF@ret_cond LF@%%1 int@0\n"											\
-	"JUMPIFEQ $substr$return LF@ret_cond bool@true\n"						\
-	"GT LF@ret_cond LF@%%1 LF@length_str\n"									\
-	"JUMPIFEQ $substr$return LF@ret_cond bool@true\n"						\
-	"EQ LF@ret_cond LF@%%2 int@0\n"											\
-	"JUMPIFEQ $substr$return LF@ret_cond bool@true\n"						\
-	"DEFVAR LF@max_n\n"													\
-	"MOVE LF@max_n LF@length_str\n"										\
-	"SUB LF@max_n LF@max_n LF@%%1\n"										\
-	"ADD LF@max_n LF@max_n int@1\n"										\
-	"DEFVAR LF@edit_n_cond\n"												\
-	"LT LF@edit_n_cond LF@%%2 int@0\n"										\
-	"JUMPIFEQ $substr$edit_n LF@edit_n_cond bool@true\n"					\
-	"GT LF@edit_n_cond LF@%%2 LF@max_n\n"									\
-	"JUMPIFEQ $substr$edit_n LF@edit_n_cond bool@true\n"					\
-	"JUMP $substr$process\n"												\
-	"LABEL $substr$edit_n\n"												\
-	"MOVE LF@%%2 LF@max_n\n"												\
-	"LABEL $substr$process\n"												\
-	"DEFVAR LF@index\n"													\
-	"MOVE LF@index LF@%%1\n"												\
-	"SUB LF@index LF@index int@1\n"										\
-	"DEFVAR LF@char\n"														\
-	"DEFVAR LF@process_loop_cond\n"										\
-	"LABEL $substr$process_loop\n"											\
-	"GETCHAR LF@char LF@%0 LF@index\n"										\
-	"CONCAT LF@%%retval LF@%%retval LF@char\n"								\
-	"ADD LF@index LF@index int@1\n"										\
-	"SUB LF@%%2 LF@%%2 int@1\n"												\
-	"GT LF@process_loop_cond LF@%%2 int@0\n"								\
-	"JUMPIFEQ $substr$process_loop LF@process_loop_cond bool@true\n"		\
-	"LABEL $substr$return\n"												\
-	"POPFRAME\n"															\
-	"RETURN\n"
-
-
-#define FUNCTION_ASC														\
-	"LABEL $asc\n"															\
-	"PUSHFRAME\n"															\
-	"DEFVAR LF@%%retval\n"													\
-	"MOVE LF@%%retval int@0\n"												\
-	"DEFVAR LF@cond_length\n"												\
-	"LT LF@cond_length LF@%%1 int@1\n"										\
-	"JUMPIFEQ $asc$return LF@cond_length bool@true\n"						\
-	"DEFVAR LF@length_str\n"												\
-	"CREATEFRAME\n"														    \
-	"DEFVAR TF@%%0\n"														\
-	"MOVE TF@%0 LF@%%0\n"													\
-	"CALL $length\n"														\
-	"MOVE LF@length_str TF@%%retval\n"										\
-	"GT LF@cond_length LF@%%1 LF@length_str\n"								\
-	"JUMPIFEQ $asc$return LF@cond_length bool@true\n"						\
-	"SUB LF@%%1 LF@%%1 int@1\n"												\
-	"STRI2INT LF@%%retval LF@%0 LF@%%1\n"									\
-	"LABEL $asc$return\n"													\
-	"POPFRAME\n"															\
-	"RETURN\n"
-
-
-#define FUNCTION_CHR														\
-	"LABEL $chr\n"															\
-	"PUSHFRAME\n"															\
-	"DEFVAR LF@%%retval\n"													\
-	"MOVE LF@%%retval string@\n"											\
-	"DEFVAR LF@cond_range\n"												\
-	"LT LF@cond_range LF@%0 int@0\n"										\
-	"JUMPIFEQ $chr$return LF@cond_range bool@true\n"						\
-	"GT LF@cond_range LF@%0 int@255\n"										\
-	"JUMPIFEQ $chr$return LF@cond_range bool@true\n"						\
-	"INT2CHAR LF@%%retval LF@%%0\n"											\
-	"LABEL $chr$return\n"													\
-	"POPFRAME\n"															\
-	"RETURN\n"
+void create_functions(){
+    fprintf(stdout,"FUNCLEN\n");
+	fprintf(stdout,"PUSHFRAME\n");
+	fprintf(stdout,"\n LABEL $length\n");
+	fprintf(stdout,"DEFVAR LF@%%retval\n");
+	fprintf(stdout,"STRLEN LF@%%retval LF@%%0\n");
+	fprintf(stdout,"POPFRAME\n");
+	fprintf(stdout,"RETURN\n\n\n");
+	fprintf(stdout,"#SUBSTRFUNCTION\n");
+	fprintf(stdout,"LABEL $substr\n");
+	fprintf(stdout,"PUSHFRAME\n");
+	fprintf(stdout,"DEFVAR LF@%%retval\n");
+	fprintf(stdout,"MOVE LF@%%retval string@\n");
+	fprintf(stdout,"DEFVAR LF@length_str\n");
+	fprintf(stdout,"CREATEFRAME\n");
+	fprintf(stdout,"DEFVAR TF@%%0\n");
+	fprintf(stdout,"MOVE TF@%%0 LF@%%0\n");
+	fprintf(stdout,"CALL $length\n");
+	fprintf(stdout,"MOVE LF@length_str TF@%%retval\n");
+	fprintf(stdout,"DEFVAR LF@ret_cond\n");
+	fprintf(stdout,"LT LF@ret_cond LF@length_str int@0\n");
+	fprintf(stdout,"JUMPIFEQ $substr$return LF@ret_cond bool@true\n");
+	fprintf(stdout,"EQ LF@ret_cond LF@length_str int@0\n");
+	fprintf(stdout,"JUMPIFEQ $substr$return LF@ret_cond bool@true\n");
+	fprintf(stdout,"LT LF@ret_cond LF@%%1 int@0\n");
+	fprintf(stdout,"JUMPIFEQ $substr$return LF@ret_cond bool@true\n");
+	fprintf(stdout,"EQ LF@ret_cond LF@%%1 int@0\n");
+	fprintf(stdout,"JUMPIFEQ $substr$return LF@ret_cond bool@true\n");
+	fprintf(stdout,"GT LF@ret_cond LF@%%1 LF@length_str\n");
+	fprintf(stdout,"JUMPIFEQ $substr$return LF@ret_cond bool@true\n");
+	fprintf(stdout,"EQ LF@ret_cond LF@%%2 int@0\n");
+	fprintf(stdout,"JUMPIFEQ $substr$return LF@ret_cond bool@true\n");
+	fprintf(stdout,"DEFVAR LF@max_n\n");
+	fprintf(stdout,"MOVE LF@max_n LF@length_str\n");
+	fprintf(stdout,"SUB LF@max_n LF@max_n LF@%%1\n");
+	fprintf(stdout,"ADD LF@max_n LF@max_n int@1\n");
+	fprintf(stdout,"DEFVAR LF@edit_n_cond\n");
+	fprintf(stdout,"LT LF@edit_n_cond LF@%%2 int@0\n");
+	fprintf(stdout,"JUMPIFEQ $substr$edit_n LF@edit_n_cond bool@true\n");
+	fprintf(stdout,"GT LF@edit_n_cond LF@%%2 LF@max_n\n");
+	fprintf(stdout,"JUMPIFEQ $substr$edit_n LF@edit_n_cond bool@true\n");
+	fprintf(stdout,"JUMP $substr$process\n");
+	fprintf(stdout,"LABEL $substr$edit_n\n");
+	fprintf(stdout,"MOVE LF@%%2 LF@max_n\n");
+	fprintf(stdout,"LABEL $substr$process\n");
+	fprintf(stdout,"DEFVAR LF@index\n");
+	fprintf(stdout,"MOVE LF@index LF@%%1\n");
+	fprintf(stdout,"SUB LF@index LF@index int@1\n");
+	fprintf(stdout,"DEFVAR LF@char\n");
+	fprintf(stdout,"DEFVAR LF@process_loop_cond\n");
+	fprintf(stdout,"LABEL $substr$process_loop\n");
+	fprintf(stdout,"GETCHAR LF@char LF@%0 LF@index\n");									
+	fprintf(stdout,"CONCAT LF@%%retval LF@%%retval LF@char\n");							
+	fprintf(stdout,"ADD LF@index LF@index int@1\n");										
+	fprintf(stdout,"SUB LF@%%2 LF@%%2 int@1\n");											
+	fprintf(stdout,"GT LF@process_loop_cond LF@%%2 int@0\n");							
+	fprintf(stdout,"JUMPIFEQ $substr$process_loop LF@process_loop_cond bool@true\n");	
+	fprintf(stdout,"LABEL $substr$return\n");										
+	fprintf(stdout,"POPFRAME\n");														
+	fprintf(stdout,"RETURN\n\n\n");
+	fprintf(stdout,"#ASCFUNCTION\n");													
+	fprintf(stdout,"LABEL $asc\n");															
+	fprintf(stdout,"PUSHFRAME\n");															
+	fprintf(stdout,"DEFVAR LF@%%retval\n");													
+	fprintf(stdout,"MOVE LF@%%retval int@0\n");												
+	fprintf(stdout,"DEFVAR LF@cond_length\n");									
+	fprintf(stdout,"LT LF@cond_length LF@%%1 int@1\n");						
+	fprintf(stdout,"JUMPIFEQ $asc$return LF@cond_length bool@true\n");
+	fprintf(stdout,"DEFVAR LF@length_str\n");												
+	fprintf(stdout,"CREATEFRAME\n");														    
+	fprintf(stdout,"DEFVAR TF@%%0\n");														
+	fprintf(stdout,"MOVE TF@%0 LF@%%0\n");													
+	fprintf(stdout,"CALL $length\n");														
+	fprintf(stdout,"MOVE LF@length_str TF@%%retval\n");										
+	fprintf(stdout,"GT LF@cond_length LF@%%1 LF@length_str\n");						
+	fprintf(stdout,"JUMPIFEQ $asc$return LF@cond_length bool@true\n");
+	fprintf(stdout,"SUB LF@%%1 LF@%%1 int@1\n");									
+	fprintf(stdout,"STRI2INT LF@%%retval LF@%0 LF@%%1\n");
+	fprintf(stdout,"LABEL $asc$return\n");
+	fprintf(stdout,"POPFRAME\n");														
+	fprintf(stdout,"RETURN\n\n\n");
+	fprintf(stdout,"#CHRFUNCTION\n");														
+	fprintf(stdout,"LABEL $chr\n");															
+	fprintf(stdout,"PUSHFRAME\n");															
+	fprintf(stdout,"DEFVAR LF@%%retval\n");													
+	fprintf(stdout,"MOVE LF@%%retval string@\n");											
+	fprintf(stdout,"DEFVAR LF@cond_range\n");									
+	fprintf(stdout,"LT LF@cond_range LF@%0 int@0\n");					
+	fprintf(stdout,"JUMPIFEQ $chr$return LF@cond_range bool@true\n");
+	fprintf(stdout,"GT LF@cond_range LF@%0 int@255\n");						
+	fprintf(stdout,"JUMPIFEQ $chr$return LF@cond_range bool@true\n");
+	fprintf(stdout,"INT2CHAR LF@%%retval LF@%%0\n");											
+	fprintf(stdout,"LABEL $chr$return\n");													
+	fprintf(stdout,"POPFRAME\n");														
+	fprintf(stdout,"RETURN\n\n");
+}
 
 //generate main function
 void generate_main()
@@ -286,44 +282,38 @@ void print_function(struct token_s *token_write, char *s)
 void generate_while_head()
 {
     fprintf(stdout, "#WHILE CYCLE\n");
-    fprintf("LABEL while\n");
+    fprintf(stdout,"LABEL while\n");
 }
 
 void generate_while_head(struct token_s *token, char *s){
-    //while true
+    //while 32
     //while ( a == b)
     //while ( a != b)   
     //while (i <= 100)
+    //while(len("huy") < 5)
+    fprintf(stdout,"DEFVAR %s@res\n",s);//res helps us to check if podminka je splnena
+    fprintf(stdout,"DEFVAR %s@returnvalueforwhile\n",s);//
     switch (token->type)
     {
-        case TOKEN_GREATER_EQ:
-            fprintf("JUMPIFEQ %s\n", token->attribute.string);            
-            break;
-        case TOKEN_GREATER:
-
-            break;
-        case//TOKEN true ci false
-            TOKEN_INT:
-            TOKEN_FLOAT:
-            //fprintf("");
-            break;
-        case TOKEN_EQUAL:
-            fprintf(stdout,"JUMPIFEQ %s\n", token->attribute.string);
-            fprintf(stdout,"LABEL to");//a == b
-            break;
-        case TOKEN_NOT_EQUAL:
-            fprintf(stdout,"JUMPIFNEQ %s\n", token->attribute.string);
-            break;
-
+    case TOKEN_FLOAT :
+        fprintf(stdout,"MOVE %s@returnvalue float@%f\n",s, token->attribute.float_val);
+        break;
+    case TOKEN_INT :
+        fprintf(stdout,"MOVE %s@returnvalue int@%d\n",s, token->attribute.int_val);
+        break;
+    case TOKEN_STRING:
+        fprintf("MOVE %s@returnvalue string@%s\n",s, token->attribute.string);
+        break;
+    case TOKEN_ID:
+        fprintf("MOVE %s@returnvalue %s@%s\n",s, s,token->attribute.string);
+        break;
     
     default:
         break;
     }
 
 }
-
 void generate_while_end(struct token_s *token){
-
     fprintf(stdout,"JUMP WHILE\n");
     fprintf(stdout,"LABEL %s\n", token->attribute.string);
 }
@@ -331,12 +321,128 @@ void generate_while_end(struct token_s *token){
 
 
 
-void generate_if_head()
-{
+void generate_if_head(){
     fprintf(stdout,"#Endif\n");
 }
-
-void generate_if_start(){
-    fprintf(stdout, "JUMPIFEQ $");
-    fprintf(stdout,"DEFVAR %sres");
+void generate_if_start(struct token_s *token, char *s){ 
+    //if a == a
+    //if a != b
+    //if a >= b
+    //if a <= b
+    //if a < b
+    //if a > b
+    fprintf(stdout,"DEFVAR %s@res\n",s);//res helps us to check if podminka je splnena
+    fprintf(stdout,"DEFVAR %s@returnvalue\n",s);
+    fprintf(stdout,"DEFVAR %s@returnvalue1\n",s);
+    switch (token->type)
+    {
+    case TOKEN_FLOAT :
+        fprintf(stdout,"MOVE %s@returnvalue float@%f\n",s, token->attribute.float_val);
+        break;
+    case TOKEN_INT :
+        fprintf(stdout,"MOVE %s@returnvalue int@%d\n",s, token->attribute.int_val);
+        break;
+    case TOKEN_STRING:
+        fprintf("MOVE %s@returnvalue string@%s\n",s, token->attribute.string);
+        break;
+    case TOKEN_ID:
+        fprintf("MOVE %s@returnvalue %s@%s\n",s, s,token->attribute.string);
+        break;
+    
+    default:
+        break;
+    }
 }
+void select_operator(struct token_s *token){
+    switch (token->type)
+    {
+    case TOKEN_EQUAL:
+        fprintf(stdout,"EQ ");
+        break;
+     case TOKEN_NOT_EQUAL:
+        fprintf(stdout,"NOT EQ ");
+        break;
+    case TOKEN_GREATER:
+        fprintf(stdout,"GT ");
+        break;
+    case TOKEN_GREATER_EQ:
+        fprintf(stdout,"EQ AND GT ");
+        break;
+    case TOKEN_LESS:
+       fprintf(stdout,"LT" );
+        break;
+    case TOKEN_LESS_EQ:
+       fprintf(stdout,"LT AND EQ ");
+        break;
+    
+    default:
+        break;
+    }
+    
+}
+void returnvalue1 (struct token_s *token, char *s){
+    switch (token->type)
+    {
+    case TOKEN_FLOAT :
+        fprintf(stdout,"MOVE %s@returnvalue1 float@%f\n",s, token->attribute.float_val);
+        break;
+    case TOKEN_INT :
+        fprintf(stdout,"MOVE %s@returnvalue1 int@%d\n",s, token->attribute.int_val);
+        break;
+    case TOKEN_STRING:
+        fprintf("MOVE %s@returnvalue1 string@%s\n",s, token->attribute.string);
+        break;
+    case TOKEN_ID:
+        fprintf("MOVE %s@returnvalue1 %s@%s\n",s, s,token->attribute.string);
+        break;
+    
+    default:
+        break;
+    }
+}
+void if_body(struct token_s *token, char *s){
+    switch (token->type)
+    {
+    case TOKEN_FLOAT:
+        fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
+        break;
+    case TOKEN_INT:
+        fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
+    case TOKEN_ID:
+        fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
+        break;
+    case TOKEN_STRING:
+        fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
+    
+    default:
+        break;
+    }
+    fprintf(stdout, "JUMPIFNEQ $EXIT %s@res boot@true",s);//if(a < b)
+                                                            //  print b
+                                                            //else
+}                                                          //  print a
+void if_else_body(struct token_s *token, char *s){
+
+    switch (token->type)
+    {
+    case TOKEN_FLOAT:
+        fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
+        break;
+    case TOKEN_INT:
+        fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
+    case TOKEN_ID:
+        fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
+        break;
+    case TOKEN_STRING:
+        fprintf(stdout,"%s@res %s@returnvalue %s@returnvalue1",s,s,s);
+    
+    default:
+        break;
+    }
+    fprintf(stdout, "JUMPIFNEQ $ELSE %s@res boot@true", s );
+}
+void if_end(){
+    fprintf(stdout,"LABEL $EXIT\n");
+}
+
+
